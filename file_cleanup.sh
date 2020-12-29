ef7eb5b73e26c73b3bf93df7c8ee9ae106e3d4b3
digital-byte <64812398+digital-byte@users.noreply.github.com>
2020-12-29 16:59:35 -0600

    Create file_cleanup.sh

---
+++ file_cleanup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Usage: cleanup_old_logs &lt;folder&gt; &lt;days&gt;
+# Removes all log files in the directory older than a certain number of days
+
+FOLDER=$1
+N_DAYS=$2
+
+# Validate
+if [ "$FOLDER" == "" ] || [ "$N_DAYS" == "" ]
+then
+echo "Usage: $0 <folder> <number_of_days>"
+exit 1
+fi
+
+if [ ! -d "$FOLDER" ]
+then
+echo "$FOLDER is not a directory"
+exit 2
+fi
+
+# Remove
+echo "Deleting files in $FOLDER older than $N_DAYS days"
+# This can be used as a oneliner in cron
+find $FOLDER/* -mtime +$N_DAYS -exec rm {} \;
